ffc2d8d31f141b8fb37f34efb73d7bf1eaa92e72
CHETANKUMAR20 <c458047@gmail.com>
2026-02-20 01:12:02 +0530

    Initial Terraform Infrastructure Setup

---
+++ backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    bucket         = "chetan-terraform-state-bucket"
+    key            = "terraform-infra/terraform.tfstate"
+    region         = "ap-south-1"
+    encrypt        = true
+  }
+}
